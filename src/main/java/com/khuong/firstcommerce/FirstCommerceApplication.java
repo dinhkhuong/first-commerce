28942d03e6547661dbe2777a4787483a55ea9191
Khuong <lieukhuong@gmail.com>
2021-12-01 10:49:13 -0500

    initial for First Commerce

---
+++ src/main/java/com/khuong/firstcommerce/FirstCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.khuong.firstcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khuong/firstcommerce/FirstCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khuong.firstcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
