cf02dc749a789ce93c2b1e29d6844060c224bf45
Khuong <lieukhuong@gmail.com>
2021-12-03 09:49:04 -0500

    simple retrieve of authenticated user object

--- src/main/java/com/khuong/firstcommerce/controller/SecurityController.java
+++ src/main/java/com/khuong/firstcommerce/controller/SecurityController.java
@@ -1,5 +1,7 @@
 package com.khuong.firstcommerce.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,11 @@ public class SecurityController {
 
     ///http://localhost:8080/home
     @GetMapping("/home")
-    public String home(){
-        return "This is Home Page";
+    public UserDetails home(Authentication authentication){
+        UserDetails userPrincipal = (UserDetails)authentication.getPrincipal();
+
+        //return "This is Home Page \n Hello " + userPrincipal.getUsername() + "";
+        return userPrincipal;
     }
 
     ///http://localhost:8080/admin
